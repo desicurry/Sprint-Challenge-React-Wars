33cf7d44c2a0bddf6c3532a189cd75cdf8847810
Desi Curry <Blackice@Desis-MacBook-Air.local>
2020-02-07 22:29:59 -0500

    MVP Complete

---
+++ starwars/src/components/CharacterListing.js
@@ -0,0 +1,61 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import StarWarsCard from './StarWarsCard';
+import {Container, Row} from 'reactstrap';
+
+
+export default function CharacterListing () {
+
+    const [person, setPerson] = useState();
+    const [url, setUrl] = useState('https://swapi.co/api/people/')
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const starWarsData = await axios.get(url);
+                setPerson(starWarsData.data);
+            }catch(err) {
+                console.log(err);
+            }
+        }
+
+        fetchData();
+    }, [url])
+
+    const handleNext = () => {
+        if(person.next){
+            setUrl(person.next)
+        }
+    }
+
+    const handlePrevious = () => {
+        if(person.previous){
+            setUrl(person.previous)
+        }
+    }
+
+
+    return (
+        <Container>
+            <button onClick={handlePrevious}>Previous</button>
+            <button onClick={handleNext}>Next</button>
+            <Row>
+            {person && person.results.map((person, index) => {
+                return (
+                <StarWarsCard
+                    key = {index}
+                    name = {person.name} 
+                    gender = {person.gender}
+                    hair = {person.hair_color}
+                    eyes = {person.eye_color}
+                    birthYear = {person.birth_year}
+                    species = {person.species}
+                    films = {person.films}
+
+                />
+                )
+            })}
+            </Row>
+        </Container>
+    );
+} 
---
+++ starwars/src/components/StarWarsCard.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { Card, CardText, CardBody, CardTitle, CardSubtitle, Col, Button } from "reactstrap";
+import axios from 'axios';
+
+export default function StarWarsCard(props) {
+
+    const [films, setFilms] = useState()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const promiseArray = props.films.map(url=> axios.get(url))
+        Promise.all( promiseArray )
+            .then(res => {
+                const filmDescriptions = res.map(film => film.data)
+                setFilms(filmDescriptions)
+                setLoading(false)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    },[props.films])
+
+    
+
+    return(
+        <Col xs='6' lg='4'>
+            <Card height='100%' body outline color = 'success'  className='text-left'>
+                <CardBody height='100%'>
+                    <CardTitle className='text-center'>{props.name}</CardTitle>
+                    <CardSubtitle className='text-center'>{props.gender}</CardSubtitle>
+                    <hr />
+                    <CardText>Birth Year: {props.birthYear}</CardText>
+                    <CardText>Eye Color: {props.eyes}</CardText>
+                    <CardText>Hair Color: {props.hair}</CardText>
+                    <CardText>Films: {loading ? 'Loading' : films && films.map(film => ` ${film.title} |`)}</CardText>
+                    <Button className='text-left'>More</Button>
+                </CardBody>
+            </Card>
+
+        </Col>
+    )
+} 
